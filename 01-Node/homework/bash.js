1afc308efe895fd68e9833e92e17f75ae94fef88
velasquezmarcelo <velasquezmarcelo96@gmail.com>
2022-11-04 19:43:17 -0300

    finished homework

--- 01-Node/homework/bash.js
+++ 01-Node/homework/bash.js
@@ -1,4 +1,49 @@
-console.log(process)
+//console.log(global.process)
 
+//process.stdout.write('tuki' + '\n') 
+
+// process.stdin.on('data', (data)=> {
+//     console.log(data)
+//     process.stdout.write(data) 
+// })
 
 //process es un modulo del objeto global
+//instancia de EventEmitter-> podemos emitir eventos
+//process contiene metodos, eventos y props
+//metodos: stdin y stdout ingresan y sacan datos con node
+//stdin escribe en la consola con el metodo on
+//stdout permite escribir cosas y mostrarlas en pantalla
+//buffer es codigo duro, nosotros noentendemos
+
+
+//este archivo hace de consola
+//instalar nodemon
+//primero debemos quedarnos con el input luego con la primer palabla que seria el "comando"
+//en el ejemplo seria: echo holis -> echo
+//debemos convertirlo en un array
+
+const commands = require ('./commands/index'); //usamos commands js porque es la forma nativa por lo cual no hay que instalar nada mas, evitamos los modulos de ECMA tendriamos que modificar json e instalar un loader
+
+const print = (input) => {
+    process.stdout.write(input + '\n') 
+    process.stdout.write('prompt > ')
+}
+
+process.stdout.write('prompt > ') 
+
+process.stdin.on('data', (data)=> {  //data es buffer
+    let args = data.toString().trim().split(' ');   //.trim elimina los espacios en blanco de los extremos
+    //'echo holis' -> ['echo', 'holi']
+    let cmd = args.shift(); //'echo'
+    //console.log(args)
+
+    //console.log(commands[cmd]())
+    //ternario
+    commands[cmd]  //commands es un objeto, cmd es una propiedad una variable, si esto da true pasa a la siguiente linea
+    ? commands[cmd](args, print)
+    : print('Command invalid ') //los : actuan como el else
+
+    // process.stdout.write(cmd + '\n') 
+    // process.stdout.write('prompt > ') //para no repetir codigo colocamos estas lineas dentro de la funcion print
+
+})
--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -1,5 +1,63 @@
+const fs = require('fs');
+const { request } = require('http');
 
+const echo = (args, print) => {
+    print (args.join(' '))  //.join es lo contrario del split
+}
+
+const pwd = (args, print) => {  //devuelve el directorio donde estamos parados
+    //print (process.mainModule.path)  //el objeto process tiene una props path
+    print(__dirname.split('\\').pop()) //forma mas simple y nos entrega solo la carpeta actual y no todo el directorio
+    //print(__dirname.split('\\').at(-1)) //hace exactamente lo mismo
+}
+
+const date = (args, print) => {  //nos entrega la fecha
+    print(Date())
+
+}
+
+const ls = (args, print) => {
+    fs.readdir('.',(err, files) => { //readdir es un metodo dentro del modulo fs nos permite leer el directorio, fs es un modulo dentro de node 
+        err
+        ? console.log(err)
+        //: (files.forEach(files => { //usamos forEach en vez de map
+            //process.stdout.write(file + '\n') //asi no queda bien visualmente
+        : print(files.join('\n')) //este es un codigo mas corto, .join transforma un array en string
+        })
+}
+
+const cat = (args, print ) => {  //es una funcion para leer archivo
+    fs.readFile(args[0], (err, data) => {  //siempre en la posicion 0 va a quedar el nombre del archivo que quiero leer
+        print(data)
+    })
+}
+
+const head = (args, print) => { //lee la primeras lineas del archivo
+    fs.readFile(args[0], 'utf-8', (err, data) => {
+        print(data.split('\n').splice(0, 5).join('\n'))
+    })
+}
+
+const tail = (args, print) => {
+    fs.readFile(args[0], 'utf-8', (err, data) => {
+        print(data.split('\n').splice(-5).join('\n'))
+    })
+}
+
+const curl = (args, print) => { //instalar npm i --save request
+    request(args[0], (err, data) => {
+        //console.log(data)
+        print(data.body)
+    })
+}
 
 module.exports = {
-
+    echo,
+    pwd,
+    date,
+    ls,
+    cat,
+    head,
+    tail,
+    curl,
 }
